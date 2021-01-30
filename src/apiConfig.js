b8cf167c48efb1fdfc13e90aebdea2d5180fc12b
RNolan19 <rnolan19@gmail.com>
2021-01-30 13:47:13 -0500

    adding CORS anwyehere access

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://fierce-journey-93874.herokuapp.com',
+  production: 'https://cors-anywhere.herokuapp.com/https://fierce-journey-93874.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
